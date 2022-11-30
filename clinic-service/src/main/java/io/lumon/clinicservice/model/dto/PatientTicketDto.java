481a5eb7051fc571a2b7a06f008a5aa4b085b0b6
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-11-30 12:56:37 +0300

    Refactoring

---
+++ clinic-service/src/main/java/io/lumon/clinicservice/model/dto/PatientTicketDto.java
@@ -0,0 +1,19 @@
+package io.lumon.clinicservice.model.dto;
+
+import io.lumon.clinicservice.type.TicketStatus;
+import io.lumon.clinicservice.type.TicketType;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class PatientTicketDto {
+    private Long assignedDoctorId;
+    private Long patientId;
+    private Long departmentId;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime appointmentTime;
+    private String description;
+    private TicketType type;
+    private TicketStatus status;
+}
